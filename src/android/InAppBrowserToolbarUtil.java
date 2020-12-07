3c75cd8c779de7302729137c636d008768dd712c
Subinkrishna Gopi <subinkrishna.gopi@gmail.com>
2020-12-07 15:35:31 -0500

    feat: added support for font/brand.ttf (or .otf)

--- src/android/InAppBrowserToolbarUtil.java
+++ src/android/InAppBrowserToolbarUtil.java
@@ -1,7 +1,10 @@
 package org.apache.cordova.inappbrowser;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.res.Resources;
+import android.graphics.Typeface;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,6 +13,8 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 public class InAppBrowserToolbarUtil {
+
+    public static final String TAG = "IAB_UTIL";
 
     public static FrameLayout createCustomToolbarView(
             Activity activity,
@@ -24,23 +29,47 @@ public class InAppBrowserToolbarUtil {
                 "in_app_browser_toolbar",
                 "layout",
                 activity.getPackageName());
-        
+
         FrameLayout customToolbar = (FrameLayout) LayoutInflater.from(parent.getContext())
                 .inflate(toolbarLayoutRes, parent, false);
         customToolbar.setBackgroundColor(backgroundColor);
 
         ImageView closeButton = (ImageView) customToolbar.getChildAt(0);
-        if (closeButton != null) {
+        if (null != closeButton) {
             closeButton.setOnClickListener(onClose);
             closeButton.setColorFilter(foregroundColor);
-        }   
+        }
 
         TextView titleView = (TextView) customToolbar.getChildAt(1);
-        if (titleView != null) {
+        if (null != titleView) {
             titleView.setText(title);
             titleView.setTextColor(foregroundColor);
+            titleView.setTypeface(getBrandTypeface(activity.getApplicationContext()));
         }
 
         return customToolbar;
+    }
+
+    private static Typeface getBrandTypeface(Context context) {
+        Typeface typeface = getTypeface(context, "fonts/brand.ttf");
+        if (null != typeface) return typeface;
+
+        typeface = getTypeface(context, "fonts/brand.otf");
+        if (null != typeface) return typeface;
+
+        Log.d(TAG, "No brand font found. Using system font.");
+        return Typeface.DEFAULT_BOLD;
+    }
+
+    private static Typeface getTypeface(
+            Context context,
+            String fileName
+    ) {
+        try {
+            return Typeface.createFromAsset(context.getAssets(), fileName);
+        } catch (Throwable t) {
+            // Ignored
+        }
+        return null;
     }
 }
